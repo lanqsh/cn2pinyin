61dc23bbbe10dffff92157ca570e4ea2c29113fc
Johnny <lanqsh@hotmail.com>
2024-08-12 18:19:59 +0800

    :tada: first commit

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
-# zhtopy
-Chinese pinyin to the first letter
+#include <string>
+#include <iostream>
+#include "zhtopy.hpp"
 
-中文字符串转首字母,主要是为了进行排序
-
-
-示例
-```
+int main()
+{
     std::string pinyin = ZhToPY::instance()->zhToPY("中文");  // pinyin : "zhongwen"
     std::string jinpin = ZhToPY::instance()->zhToJP("中文");  // jinpin : "ZW"
     std::string shouzimu = ZhToPY::instance()->zhToZM("中文");   // shouzimu: "Z"
     std::string abc = ZhToPY::instance()->zhToZM("abc");   // shouzimu: "a"
-    std::string abc1 = ZhToPY::instance()->zhToZM("1");   // shouzimu: "1"  
-```
+    std::string abc1 = ZhToPY::instance()->zhToZM("1");   // shouzimu: "1"
+
+    std::cout << jinpin << std::endl;
+    std::cout << shouzimu << std::endl;
+    std::cout << abc << std::endl;
+    std::cout << abc1 << std::endl;
+    return 0;
+}
